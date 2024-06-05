3f7207ab9a18fa58823373c3f8267385c98fd1aa
unknown <32268498+DimitarMilushev@users.noreply.github.com>
2024-06-05 21:26:12 +0300

    initial commit

---
+++ src/test/java/com/d_m/noted/NotedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d_m.noted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
